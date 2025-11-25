e927793729b6db8944e6a41e2ece12644ba83d77
harrish <150880592+harrish1709@users.noreply.github.com>
2025-11-25 15:38:15 +0530

    Update amitretail_scraper.py

--- backend/scrapers/amitretail_scraper.py
+++ backend/scrapers/amitretail_scraper.py
@@ -8,7 +8,7 @@ import random
 def scrape_amitretail(brand, product, oem_number=None, asin_number=None):
     # Start undetected Chrome (headless OK!)
     options = uc.ChromeOptions()
-    options.headless = True
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
@@ -23,10 +23,11 @@ def scrape_amitretail(brand, product, oem_number=None, asin_number=None):
     
     options.add_argument(f"--user-agent={random.choice(user_agents)}")
 
-
-    driver = uc.Chrome(version_main=141, options=options)
+    driver = None
 
     try:
+        driver = uc.Chrome(options=options)
+        
         polite_delay()
 
         # Build search query
