017ab235261481bc644f7f68225520c1932500bd
harrish <150880592+harrish1709@users.noreply.github.com>
2025-11-27 11:45:28 +0530

    Update sharafdg_scraper.py

--- backend/scrapers/sharafdg_scraper.py
+++ backend/scrapers/sharafdg_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import time, re, os, zipfile, random, string
 from datetime import datetime
 from scrapers.utils import polite_delay, save_to_excel
-
+import gc
 # --- PROXY CONFIGURATION ---
 PROXY_HOST = "gate.decodo.com"  # Check your dashboard
 PROXY_PORT = "10001"             # Check your dashboard
@@ -205,3 +205,5 @@ def scrape_sharafdg(brand, product, oem_number=None, asin_number=None):
                 os.remove(proxy_plugin)
             except:
                 pass
+        gc.collect()
+
