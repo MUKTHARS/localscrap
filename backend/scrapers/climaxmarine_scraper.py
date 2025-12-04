2a1d3d7a3b638c9dd654d58d354d20890d686e8e
harrish <150880592+harrish1709@users.noreply.github.com>
2025-12-04 21:48:42 +0530

    Update climaxmarine_scraper.py

--- backend/scrapers/climaxmarine_scraper.py
+++ backend/scrapers/climaxmarine_scraper.py
@@ -92,10 +92,10 @@ def scrape_climaxmarine(brand, product, oem_number=None, asin_number=None):
 
         driver.get(url)
 
-        # Auto-scroll to load everything
-        for _ in range(4):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-            time.sleep(1)
+        # # Auto-scroll to load everything
+        # for _ in range(4):
+        #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        #     time.sleep(1)
 
         soup = BeautifulSoup(driver.page_source, "html.parser")
 
