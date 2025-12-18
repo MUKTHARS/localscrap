f10cb888929d613f4a9f95ea3760fda0cfd0deb5
harrish <150880592+harrish1709@users.noreply.github.com>
2025-12-18 15:16:33 +0530

    Update flipkart_scraper.py

--- backend/scrapers/flipkart_scraper.py
+++ backend/scrapers/flipkart_scraper.py
@@ -127,9 +127,9 @@ def scrape_flipkart(brand, product, oem_number=None, asin_number=None):
         
         driver.get(url)
         
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-id]"))
-        )
+        # WebDriverWait(driver, 5).until(
+        #     EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-id]"))
+        # )
 
         soup = BeautifulSoup(driver.page_source, "html.parser")
         product_cards = soup.select("div[data-id]")
